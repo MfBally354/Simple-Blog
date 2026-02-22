7f33ba32a5189c776ba4c4098ea6220b3dba2223
MfBally354 <iqbalguntur313@gmail.com>
2026-02-22 17:45:50 +0700

    Auto commit

--- admin/articles/delete.php
+++ admin/articles/delete.php
@@ -1,3 +1,15 @@
 <?php
 // File: admin/articles/delete.php
-?>
+require_once dirname(dirname(__DIR__)) . '/includes/session.php';
+require_once dirname(dirname(__DIR__)) . '/includes/config.php';
+require_once dirname(dirname(__DIR__)) . '/includes/functions.php';
+requireLogin();
+
+$id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
+if ($id && getArticleById($id)) {
+    deleteArticle($id);
+    setFlash('success', 'Artikel berhasil dihapus.');
+} else {
+    setFlash('error', 'Artikel tidak ditemukan.');
+}
+redirect(SITE_URL . '/admin/articles/');
