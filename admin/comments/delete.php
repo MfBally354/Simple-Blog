920ff37909bf4e913de02471fb49801938eee135
MfBally354 <iqbalguntur313@gmail.com>
2026-02-22 17:47:44 +0700

    Auto commit

--- admin/comments/delete.php
+++ admin/comments/delete.php
@@ -1,3 +1,13 @@
 <?php
 // File: admin/comments/delete.php
-?>
+require_once dirname(dirname(__DIR__)) . '/includes/session.php';
+require_once dirname(dirname(__DIR__)) . '/includes/config.php';
+require_once dirname(dirname(__DIR__)) . '/includes/functions.php';
+requireLogin();
+
+$id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
+if ($id) {
+    deleteComment($id);
+    setFlash('success', 'Komentar berhasil dihapus.');
+}
+redirect(SITE_URL . '/admin/comments/');
