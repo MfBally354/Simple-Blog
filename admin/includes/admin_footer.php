1a4b1d4e0bdc5116a160b8879887105ac0018159
MfBally354 <iqbalguntur313@gmail.com>
2026-02-22 17:44:24 +0700

    Auto commit

--- admin/includes/admin_footer.php
+++ admin/includes/admin_footer.php
@@ -1,3 +1,11 @@
 <?php
 // File: admin/includes/admin_footer.php
 ?>
+        </div><!-- /.container-fluid -->
+    </div><!-- /#page-content-wrapper -->
+</div><!-- /#wrapper -->
+
+<script src="<?= SITE_URL ?>/assets/js/bootstrap.min.js"></script>
+<script src="<?= SITE_URL ?>/assets/js/admin.js"></script>
+</body>
+</html>
