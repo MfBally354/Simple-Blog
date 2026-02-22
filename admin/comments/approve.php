a9cac590065f55b3f0d2e3a3442624d961a3c8ea
MfBally354 <iqbalguntur313@gmail.com>
2026-02-22 17:47:22 +0700

    Auto commit

--- admin/comments/approve.php
+++ admin/comments/approve.php
@@ -1,3 +1,13 @@
 <?php
 // File: admin/comments/approve.php
-?>
+require_once dirname(dirname(__DIR__)) . '/includes/session.php';
+require_once dirname(dirname(__DIR__)) . '/includes/config.php';
+require_once dirname(dirname(__DIR__)) . '/includes/functions.php';
+requireLogin();
+
+$id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
+if ($id) {
+    approveComment($id);
+    setFlash('success', 'Komentar berhasil disetujui.');
+}
+redirect(SITE_URL . '/admin/comments/');
