58f57475f6362575aad73835e5ec0feb8e4f694c
MfBally354 <iqbalguntur313@gmail.com>
2026-02-22 17:43:52 +0700

    Auto commit

--- admin/logout.php
+++ admin/logout.php
@@ -1,3 +1,8 @@
 <?php
 // File: admin/logout.php
-?>
+require_once dirname(__DIR__) . '/includes/session.php';
+require_once dirname(__DIR__) . '/includes/config.php';
+require_once dirname(__DIR__) . '/includes/functions.php';
+
+session_destroy();
+redirect(SITE_URL . '/admin/login.php');
