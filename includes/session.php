fb0f6c3bcc2e6a87d8d9e0b15ea5c99c801a2dbb
MfBally354 <iqbalguntur313@gmail.com>
2026-02-22 17:39:33 +0700

    Auto commit

--- includes/session.php
+++ includes/session.php
@@ -1,3 +1,12 @@
 <?php
 // File: includes/session.php
-?>
+if (session_status() === PHP_SESSION_NONE) {
+    session_set_cookie_params([
+        'lifetime' => 86400,
+        'path'     => '/',
+        'secure'   => false,
+        'httponly' => true,
+        'samesite' => 'Lax'
+    ]);
+    session_start();
+}
