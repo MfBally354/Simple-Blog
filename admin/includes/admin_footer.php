e6a42dff0280468abbe0f0feab6bcbd39b1da093
MfBally354 <iqbalguntur313@gmail.com>
2026-03-02 11:03:23 +0700

    Auto commit

--- admin/includes/admin_footer.php
+++ admin/includes/admin_footer.php
@@ -5,7 +5,7 @@
     </div><!-- /#page-content-wrapper -->
 </div><!-- /#wrapper -->
 
-<script src="<?= SITE_URL ?>/assets/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 <script src="<?= SITE_URL ?>/assets/js/admin.js"></script>
 </body>
 </html>
