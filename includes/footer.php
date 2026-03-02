f89d8d966e104ecfbcb33c5f95b21a6cdefc36cb
MfBally354 <iqbalguntur313@gmail.com>
2026-03-02 11:02:55 +0700

    Auto commit

--- includes/footer.php
+++ includes/footer.php
@@ -47,7 +47,7 @@
     </div>
 </footer>
 
-<script src="<?= SITE_URL ?>/assets/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 <script src="<?= SITE_URL ?>/assets/js/script.js"></script>
 </body>
 </html>
