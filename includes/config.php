0b0f7bebdb7b00486006dbb2f1fcc1fe252a9485
MfBally354 <iqbalguntur313@gmail.com>
2026-03-03 21:15:55 +0700

    Auto commit

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 // File: includes/config.php
 
 // Database Configuration
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'localhost'); 
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'blog_sederhana');
