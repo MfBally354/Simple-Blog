c4fecb9d40683edfa0a6411b2531510a61a9ee05
MfBally354 <iqbalguntur313@gmail.com>
2026-03-03 21:18:52 +0700

    Auto commit

--- includes/config.php
+++ includes/config.php
@@ -9,7 +9,7 @@ define('DB_NAME', 'blog_sederhana');
 
 // Site Configuration
 define('SITE_NAME', 'Blog Sederhana');
-define('SITE_URL', 'http://localhost/Sim');
+define('SITE_URL', 'http://localhost/Simp');
 define('SITE_DESCRIPTION', 'Blog sederhana dengan PHP Native dan MySQL');
 
 // Auto-detect SITE_URL — tidak perlu diubah manual
